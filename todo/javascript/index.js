8790d90477536838d709c6052c7b7c651fb56409
nachetons <tripleignacio@gmail.com>
2021-11-22 13:32:35 +0100

    fixed styles, intento de envio de una variable en otro html

--- todo/javascript/index.js
+++ todo/javascript/index.js
@@ -141,12 +141,6 @@ document.onclick = function(e){
 
 };
 
-
-  
-
-
-
-
 //Funcion carrusel header
 
 /*for (let index = 0; index < 11; index++) {
@@ -156,44 +150,7 @@ document.onclick = function(e){
 }*/
 
 
-var slideIndex = 0;
-
-showSlides();
-
-function showSlides() {
-       var i;
-       var slides = document.getElementsByClassName("mySlides");
-
-       for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        
-       }
-
-       slideIndex++;
-       if(slideIndex > slides.length) {slideIndex = 1}
-       slides[slideIndex-1].style.display = "block";
-       setTimeout(showSlides,2500);
-}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
